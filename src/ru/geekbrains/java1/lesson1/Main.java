e21aceccdb79350eb4ada963897bfac3cad0cfd3
unknown <zlovred12@gmail.com>
2022-08-03 15:14:30 +0300

    lesson1

--- src/ru/geekbrains/java1/lesson1/Main.java
+++ src/ru/geekbrains/java1/lesson1/Main.java
@@ -2,8 +2,63 @@ package ru.geekbrains.java1.lesson1;
 
 public class Main {
 
+    private static void init(){
+        byte b = 1;
+        short s = 2;
+        int i = 3;
+        long l = 41111111111111l;
+
+        float f = 1.1f;
+        double d = 1.1;
+
+        boolean bool = true;
+        char c = 'c';
+    }
+
+    private static float calculate(float a, float b, float c, float d){
+        return a * (b + (c / d));
+    }
+
+    private static boolean checkSum(int first, int second){
+        return (first + second) >= 10 && (first + second) <= 20;
+    }
+
+    private static void lessOrGreaterZero(int number){
+        if (number >= 0){
+            System.out.println("positive");
+        }else {
+            System.out.println("negative");
+        }
+    }
+
+    private static boolean numberTransfer(int number){
+        if (number < 0){
+            return true;
+        }
+        return false;
+    }
+
+    private static void hello(String name){
+        System.out.println("Hello " + name);
+    }
+
+    private static void leapYear(int year){
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+            System.out.println("leap");
+        }else {
+            System.out.println("no leap");
+        }
+    }
+
+
 
     public static void main(String[] args) {
-        
+        System.out.println(calculate(1.0f, 1.1f, 3, 6));
+        System.out.println(checkSum(0 , 9));
+        lessOrGreaterZero(-1);
+        System.out.println(numberTransfer(-1));
+        hello("name");
+        leapYear(2003);
+
     }
 }
